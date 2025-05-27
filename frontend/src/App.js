0fb9c4cded2cf80177a5f31cdcf3ca56bb15eaa9
MuhammadApriansyahSaputra <123220142@student.upnyk.ac.id>
2025-05-27 23:29:00 +0700

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { AuthProvider, useAuthContext } from "./auth/AuthProvider";
 import AdminDashboard from "./pages/AdminDashboard";
+import Dashboard from "./pages/Dashboard.js";
 import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import KonserApp from "./pages/Konser";
-import AdminKonser from "./pages/AdminKonser";
-import AdminTiket from "./pages/AdminTiket";
-import KonserDetail from "./pages/KonserDetail";
+import RegisterPage from "./pages/RegisterPage.js";
+import AdminKonser from "./pages/AdminKonser.js";
+import AdminTiket from "./pages/AdminTiket.js";
+import Detail from "./pages/Details.js";
 
 function App() {
   return (
@@ -27,11 +27,6 @@ function AppRoutes() {
       <Route path="/" element={<LoginPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-
-      <Route
-        path="/dashboard"
-        element={isAuthenticated ? <KonserApp /> : <Navigate to="/login" />}
-      />
       <Route
         path="/admin-dashboard"
         element={
@@ -40,17 +35,20 @@ function AppRoutes() {
       />
       <Route
         path="/AdminKonser"
-        element={
-          isAuthenticated ? <AdminKonser /> : <Navigate to="/login" />
-        }
+        element={isAuthenticated ? <AdminKonser /> : <Navigate to="/login" />}
       />
       <Route
-        path="/AdminTiket"
-        element={
-          isAuthenticated ? <AdminTiket /> : <Navigate to="/login" />
-        }
+        path="/AdminTIket"
+        element={isAuthenticated ? <AdminTiket /> : <Navigate to="/login" />}
       />
-      <Route path="/konser/:id" element={<KonserDetail />} />
+      <Route
+        path="/dashboard"
+        element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />}
+      />
+      <Route
+        path="/dashboard/:id"
+        element={isAuthenticated ? <Detail /> : <Navigate to="/login" />}
+      />
     </Routes>
   );
 }
